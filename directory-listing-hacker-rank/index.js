51c1f19e2c8c60db40f104c08847b98574c1b482
Victor Mussio <victorsm9@hotmail.com>
2023-07-26 00:59:04 -0300

    fix tests

--- directory-listing-hacker-rank/index.js
+++ directory-listing-hacker-rank/index.js
@@ -1,73 +1,54 @@
-const fs = require('fs')
-const path = require('path')
-//const readline = require('readline')
+const fs = require('fs');
+const path = require('path');
+const utils = require('./test/utils')
 
-function getMetadata(pathToCheck){
+function getMetadata(pathToCheck) {
   return new Promise((resolve, reject) => {
     fs.stat(pathToCheck, (err, stats) => {
-      if(err)
-        reject(new Error('Invalid Path'))
-      else{
-        const isDirectory = stats.isDirectory()
+      if (err)
+        reject(new Error('Invalid Path'));
+      else {
+        const isDirectory = stats.isDirectory();
 
-        if(!isDirectory)
-          resolve([pathToCheck])
+        if (!isDirectory)
+          resolve([pathToCheck]);
         else
           fs.readdir(pathToCheck, (err, files) => {
-            if(err)
-              reject(new Error('Invalid Path'))
-            else{
-              const fileMetadata = files.map(file => path.join(pathToCheck, file))
-              resolve(fileMetadata)
+            if (err)
+              reject(new Error('Invalid Path'));
+            else {
+              const fileMetadata = files.map(file => path.join(pathToCheck, file));
+              resolve(fileMetadata);
             }
-          })
+          });
       }
-    })
-  })
+    });
+  });
 }
 
-function printMetadata(metadata){
-  const promises = metadata.map(getMetadata)
+function printMetadata(metadata) {
+  const promises = metadata.map(getMetadata);
 
   return Promise.all(promises)
-  .then(results => {
-    const flatResults = results.flat()
-    const metadataList = flatResults.map(filePath => {
-      const stats = fs.statSync(filePath)
+    .then(results => {
+      const flatResults = results.flat();
+      const metadataList = flatResults.map(filePath => {
+        const stats = fs.statSync(filePath);
 
-      return {
-        fileName: path.basename(filePath),
-        filePath: path.relative(process.cwd(), filePath),
-        size: stats.size,
-        createdAt: stats.birthtime.toISOString().split("T")[0],
-        isDirectory: stats.isDirectory()
-      }
+        return {
+          fileName: path.basename(filePath),
+          filePath: path.relative(process.cwd(), filePath),
+          size: stats.size,
+          createdAt: utils.dateFormat(stats.birthtime.toISOString().split("T")[0]),
+          isDirectory: stats.isDirectory()
+        };
+      });
+      return metadataList;
     })
-
-    console.log(JSON.stringify(metadataList, null, 2))
-
-    return metadataList
-    //rl.close()
-  })
-  .catch(error => {
-    console.log(error.message)
-
-    throw new Error(error.message)
-    //rl.close()
-  })
+    .catch(error => {
+      throw new Error(error.message);
+    });
 }
-
-
-//const rl = readline.createInterface({
-  //input: process.stdin,
-  //output: process.stdout
-//})
-
-//const main = () => {
-  //rl.question("Type a path :", (pathToCheck) => {
-    //printMetadata([pathToCheck])
-  //})
-//};
 
 const main = (pathToCheck) => {
   return printMetadata([pathToCheck])
--- directory-listing-hacker-rank/test/utils.js
+++ directory-listing-hacker-rank/test/utils.js
@@ -3,160 +3,174 @@ const ROOT = require('path').resolve('.');
 const fs = BlueBird.promisifyAll(require("fs"));
 
 const result = [
-    {
-        fileName: 'bla.txt',
-        filePath: '/tmp/test_dir/bla.txt',
-        size: 1850,
-        isDirectory: false,
-        createdAt: '28-02-2020'
-    },
-    {
-        fileName: 'important-data.json',
-        filePath: '/tmp/test_dir/important-data.json',
-        size: 11846,
-        isDirectory: false,
-        createdAt: '28-02-2020'
-    },
-    {
-        fileName: 'test.txt',
-        filePath: '/tmp/test_dir/test.txt',
-        size: 686,
-        isDirectory: false,
-        createdAt: '28-02-2020'
-    }
+	{
+		fileName: 'bla.txt',
+		filePath: 'tmp/test_dir/bla.txt',
+		size: 9,
+		isDirectory: false,
+		createdAt: '26-07-2023'
+	},
+	{
+		fileName: 'important-data.json',
+		filePath: 'tmp/test_dir/important-data.json',
+		size: 23,
+		isDirectory: false,
+		createdAt: '26-07-2023'
+	},
+	{
+		fileName: 'test.txt',
+		filePath: 'tmp/test_dir/test.txt',
+		size: 4,
+		isDirectory: false,
+		createdAt: '26-07-2023'
+	}
 ];
 
 let tmp1 = [
-    {
-        fileName: 'file1.txt',
-        filePath: ROOT + '/tmp1/file1.txt',
-        path: '/tmp1/file1.txt',
-        contents: 'FASTEST INTERNET SPEED',
-        isDirectory: false
-    },
-    {
-        fileName: 'file2.txt',
-        filePath: ROOT + '/tmp1/file2.txt',
-        path: '/tmp1/file2.txt',
-        contents: 'FASTEST INTERNET SPEED\nFASTEST INTERNET SPEED AGAIN\nFASTEST INTERNET SPEED AGAIN AGAIN',
-        isDirectory: false
-    },
-    {
-        isDirectory: true,
-        fileName: 'logs',
-        filePath: ROOT + '/tmp1/logs',
-        path: '/tmp1/logs',
-        children: [
-            {
-                fileName: 'file3.txt',
-                filePath: ROOT + '/tmp1/logs/file3.txt',
-                contents: 'FASTEST INTERNET SPEED\nFASTEST INTERNET SPEED AGAIN\nFASTEST AGAIN AGAIN',
-                isDirectory: false
-            },
-            {
-                fileName: 'file4.txt',
-                filePath: ROOT + '/tmp1/logs/file5.txt',
-                contents: 'F',
-                isDirectory: false
-            }
-        ]
-    },
-    {
-        fileName: 'file4.txt',
-        filePath: ROOT + '/tmp1/file4.txt',
-        path: '/tmp1/file4.txt',
-        contents: 'FASTE',
-        isDirectory: false
-    }
+	{
+		fileName: 'file1.txt',
+		filePath: 'tmp1/file1.txt',
+		path: 'tmp1/file1.txt',
+		contents: 'FASTEST INTERNET SPEED',
+		isDirectory: false
+	},
+	{
+		fileName: 'file2.txt',
+		filePath: 'tmp1/file2.txt',
+		path: 'tmp1/file2.txt',
+		contents: 'FASTEST INTERNET SPEED\nFASTEST INTERNET SPEED AGAIN\nFASTEST INTERNET SPEED AGAIN AGAIN',
+		isDirectory: false
+	},
+	{
+		isDirectory: true,
+		fileName: 'logs',
+		filePath: 'tmp1/logs',
+		path: 'tmp1/logs',
+		children: [
+			{
+				fileName: 'file3.txt',
+				filePath: 'tmp1/logs/file3.txt',
+				contents: 'FASTEST INTERNET SPEED\nFASTEST INTERNET SPEED AGAIN\nFASTEST AGAIN AGAIN',
+				isDirectory: false
+			},
+			{
+				fileName: 'file4.txt',
+				filePath: 'tmp1/logs/file5.txt',
+				contents: 'F',
+				isDirectory: false
+			}
+		]
+	},
+	{
+		fileName: 'file4.txt',
+		filePath: 'tmp1/file4.txt',
+		path: 'tmp1/file4.txt',
+		contents: 'FASTE',
+		isDirectory: false
+	}
 ];
 
 const removeFile = (pathData) => {
-    return new Promise((resolve, reject) => {
-        if (pathData.isDirectory) {
-            BlueBird.map(pathData.children, p => fs.unlinkAsync(p.filePath))
-                .then(() => fs.rmdirAsync(pathData.filePath))
-                .then(resolve)
-                .catch(reject);
-        } else {
-            fs.unlinkAsync(pathData.filePath)
-                .then(resolve)
-                .catch(reject);
-        }
-    })
+	return new Promise((resolve, reject) => {
+		if (pathData.isDirectory) {
+			BlueBird.map(pathData.children, p => fs.unlinkAsync(p.filePath))
+				.then(() => fs.rmdirAsync(pathData.filePath))
+				.then(resolve)
+				.catch(reject);
+		} else {
+			fs.unlinkAsync(pathData.filePath)
+				.then(resolve)
+				.catch(reject);
+		}
+	})
 };
 
+function createFiles() {
+	result.forEach(file => {
+		fs.writeFile(file.filePath, '', (err) => {
+			if (err) {
+				console.error('Error creating file:', file.filePath);
+			} else {
+				console.log('File created successfully:', file.filePath);
+			}
+		});
+	});
+}
+
 const cleanUp = () => {
-    return new Promise((resolve, reject) => {
-        BlueBird
-            .map(tmp1, (pathPair) => {
-                return removeFile(pathPair);
-            })
-            .then(() => fs.rmdirAsync(ROOT + '/tmp1'))
-            .then(resolve)
-            .catch(reject);
-    })
+	return new Promise((resolve, reject) => {
+		BlueBird
+			.map(tmp1, (pathPair) => {
+				return removeFile(pathPair);
+			})
+			.then(() => fs.rmdirAsync(ROOT + '/tmp1'))
+			.then(resolve)
+			.catch(reject);
+	})
 };
 
 const createDIR = (FOLDER) => {
-    return new Promise((resolve, reject) => {
-        fs.mkdir(FOLDER, err => {
-            if (err && err.code === 'EEXIST') return resolve();
-            if (err) return reject(err);
-            resolve();
-        })
-    })
+	return new Promise((resolve, reject) => {
+		fs.mkdir(FOLDER, err => {
+			if (err && err.code === 'EEXIST') return resolve();
+			if (err) return reject(err);
+			resolve();
+		})
+	})
 };
 
 const createFile = (pathData) => {
-    return new Promise((resolve, reject) => {
-        if (pathData.isDirectory) {
-            createDIR(pathData.filePath)
-                .then(() => BlueBird.mapSeries(pathData.children, c => createFile(c)))
-                .then(m => resolve(m[0]))
-                .catch(reject)
-        } else {
-            fs.writeFileAsync(pathData.filePath, pathData.contents, 'utf8')
-                .then(() => fs.statAsync(pathData.filePath))
-                .then(resolve)
-                .catch(reject)
-        }
-    })
+	return new Promise((resolve, reject) => {
+		if (pathData.isDirectory) {
+			createDIR(pathData.filePath)
+				.then(() => BlueBird.mapSeries(pathData.children, c => createFile(c)))
+				.then(m => resolve(m[0]))
+				.catch(reject)
+		} else {
+			fs.writeFileAsync(pathData.filePath, pathData.contents, 'utf8')
+				.then(() => fs.statAsync(pathData.filePath))
+				.then(resolve)
+				.catch(reject)
+		}
+	})
 };
 
 const generateTempDirectory = () => {
-    return new Promise((resolve, reject) => {
-        createDIR(ROOT + '/tmp1')
-            .then(() => BlueBird.mapSeries(tmp1, m => createFile(m)))
-            .then(stats => {
-                tmp1 = tmp1.map((f, i) => {
-                    f.size = stats[i].size;
-                    f.createdAt = dateFormat(stats[i].ctimeMs);
-                    return f;
-                });
-                resolve(tmp1);
-            })
-            .catch(reject);
-    })
+	return new Promise((resolve, reject) => {
+		createDIR(ROOT + '/tmp1')
+			.then(() => BlueBird.mapSeries(tmp1, m => createFile(m)))
+			.then(stats => {
+				tmp1 = tmp1.map((f, i) => {
+					f.size = stats[i].size;
+					f.createdAt = dateFormat(stats[i].ctimeMs);
+					return f;
+				});
+				resolve(tmp1);
+			})
+			.catch(reject);
+	})
 };
 
 const dateFormat = (timestamp) => {
-    var date = new Date(timestamp);
-    var dd = date.getUTCDate();
+	var date = new Date(timestamp);
+	var dd = date.getUTCDate();
 
-    var mm = date.getMonth() + 1;
-    var yyyy = date.getFullYear();
-    if (dd < 10) {
-        dd = '0' + dd;
-    }
+	var mm = date.getMonth() + 1;
+	var yyyy = date.getFullYear();
+	if (dd < 10) {
+		dd = '0' + dd;
+	}
 
-    if (mm < 10) {
-        mm = '0' + mm;
-    }
-    return `${dd}-${mm}-${yyyy}`;
+	if (mm < 10) {
+		mm = '0' + mm;
+	}
+	return `${dd}-${mm}-${yyyy}`;
 };
 
 module.exports = {
-    cleanUp,
-    result,
-    generateTempDirectory
+	cleanUp,
+	result,
+	createFiles,
+	generateTempDirectory,
+	dateFormat
 };
